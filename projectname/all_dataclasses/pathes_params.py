766907028bf852d5fe48c576ecb54002679aae69
DedPerded <lightman76@yandex.ru>
2022-05-09 14:11:30 +0300

    deleted files

--- projectname/all_dataclasses/pathes_params.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass()
-class PathesParams:
-    input_data_path: str
-    output_model_path: str
-    metric_path: str
-    data_url: str
--- projectname/all_dataclasses/predict_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass()
-class PredictParams:
-    path_to_data: str
-    path_to_model: str
-    output_path: str
